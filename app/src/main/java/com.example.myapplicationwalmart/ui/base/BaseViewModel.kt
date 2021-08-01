16fe5699a0e6cd24e4a53b26f8744e0f604758a3
Raveena1 <raveena.verma22@gmail.com>
2021-08-01 14:51:01 +0530

    Update BaseViewModel.kt

--- app/src/main/java/com.example.myapplicationwalmart/ui/base/BaseViewModel.kt
+++ app/src/main/java/com.example.myapplicationwalmart/ui/base/BaseViewModel.kt
@@ -1,1 +1,11 @@
 
+package com.example.myapplicationwalmart.ui.base
+
+import androidx.lifecycle.ViewModel
+import com.example.myapplicationwalmart.repository.BaseRepository
+
+abstract class BaseViewModel(
+    private val repository: BaseRepository
+) : ViewModel() {
+
+}
