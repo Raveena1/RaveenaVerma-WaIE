3ddecce5966b40f8fcd6afc7b6b4ccc691dee2a7
Raveena1 <raveena.verma22@gmail.com>
2021-08-01 14:58:23 +0530

    Update ApodApi.kt

--- app/src/main/java/com.example.myapplicationwalmart/data/network/ApodApi.kt
+++ app/src/main/java/com.example.myapplicationwalmart/data/network/ApodApi.kt
@@ -1,1 +1,12 @@
 
+package com.example.myapplicationwalmart.network
+
+import com.example.myapplicationwalmart.responses.APODData
+import retrofit2.http.GET
+
+interface ApodApi {
+    @GET("apod?api_key=Lfm2cMoBOrhMuvwamzs0W1m3zz0alFZhOpp6kdA3")
+    suspend fun getApodData(
+    ): APODData
+
+}
