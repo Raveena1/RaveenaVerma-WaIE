3a313f42b5f50e8a2941ac15658dc985b0616483
Raveena1 <raveena.verma22@gmail.com>
2021-08-01 14:38:23 +0530

    Update ApodActivity
    This is Main Activity

--- app/src/main/java/com.example.myapplicationwalmart/ui/apod/ApodActivity.kt
+++ app/src/main/java/com.example.myapplicationwalmart/ui/apod/ApodActivity.kt
@@ -1,1 +1,12 @@
 
+package com.example.myapplicationwalmart
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ApodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apod)
+    }
+}
