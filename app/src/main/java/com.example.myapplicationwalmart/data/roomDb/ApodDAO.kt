fa6292b0d0e7856de0a2819a3b71d88b4f34a4c3
Raveena1 <raveena.verma22@gmail.com>
2021-08-01 14:55:57 +0530

    Update ApodDAO.kt

--- app/src/main/java/com.example.myapplicationwalmart/data/roomDb/ApodDAO.kt
+++ app/src/main/java/com.example.myapplicationwalmart/data/roomDb/ApodDAO.kt
@@ -1,1 +1,23 @@
 
+package com.example.walmartproject.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface ApodDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertApodData(data : ApodEntity)
+    @Update
+    suspend fun updateApodData(data : ApodEntity)
+
+    @Delete
+    suspend fun deleteApodData(data : ApodEntity)
+
+    @Query("DELETE from apod_database")
+    suspend fun deleteAll()
+
+    @Query("SELECT * from apod_database")
+    fun getCompleteData() : LiveData<List<ApodEntity>>
+}
