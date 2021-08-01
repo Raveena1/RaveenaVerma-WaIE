107d3dd8ebc85ae3e28935d485c1dd922f183c68
Raveena1 <raveena.verma22@gmail.com>
2021-08-01 15:17:04 +0530

    Update ApodRepository.kt

--- app/src/main/java/com.example.myapplicationwalmart/data/repository/ApodRepository.kt
+++ app/src/main/java/com.example.myapplicationwalmart/data/repository/ApodRepository.kt
@@ -1,1 +1,18 @@
 
+package com.example.myapplicationwalmart.repository
+
+import com.example.myapplicationwalmart.network.ApodApi
+
+class ApodRepository(
+    private val api: ApodApi,
+
+    ) : BaseRepository() {
+
+    suspend fun getApodData(
+
+    ) = safeApiCall {
+        api.getApodData()
+    }
+
+
+}
