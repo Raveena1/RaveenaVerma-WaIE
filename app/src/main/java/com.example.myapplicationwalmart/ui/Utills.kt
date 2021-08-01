4886d6301848b389136029893c3f22c1c2b04608
Raveena1 <raveena.verma22@gmail.com>
2021-08-01 15:33:10 +0530

    Update Utills.kt

--- app/src/main/java/com.example.myapplicationwalmart/ui/Utills.kt
+++ app/src/main/java/com.example.myapplicationwalmart/ui/Utills.kt
@@ -1,1 +1,38 @@
+package com.example.myapplicationwalmart.ui
 
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.myapplicationwalmart.R
+import com.example.myapplicationwalmart.network.Resource
+import com.google.android.material.snackbar.Snackbar
+
+
+fun View.visible(isVisible: Boolean) {
+
+    visibility = if (isVisible) View.VISIBLE else View.GONE
+}
+
+fun View.snackbar(message: String, action: (() -> Unit)? = null) {
+    val snackbar = Snackbar.make(this, message, Snackbar.LENGTH_LONG)
+    action?.let { snackbar.setAction("Retry") { it() } }
+    snackbar.show()
+}
+
+fun Fragment.handleApiError(
+    failure: Resource.Failure,
+    retry: (() -> Unit)? = null
+) {
+    when {
+        failure.isNetworkError -> requireView().snackbar(
+            getString(R.string.network_error_msg),
+            retry
+        )
+        failure.errorCode == 401 -> {
+        }
+        else -> {
+            val error = failure.errorBody?.string().toString()
+            requireView().snackbar(error)
+        }
+    }
+
+}
