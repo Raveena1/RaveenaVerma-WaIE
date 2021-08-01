6b91953a701f4e4ba7c21b70878e57f39f1d29ed
Raveena1 <raveena.verma22@gmail.com>
2021-08-01 15:39:11 +0530

    Update

--- app/src/main/java/com.example.myapplicationwalmart/ui/base/ViewModelFactory.kt
+++ app/src/main/java/com.example.myapplicationwalmart/ui/base/ViewModelFactory.kt
@@ -1,1 +1,21 @@
 
+package com.example.myapplicationwalmart.ui.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.myapplicationwalmart.repository.ApodRepository
+import com.example.myapplicationwalmart.repository.BaseRepository
+import com.example.myapplicationwalmart.ui.apod.ApodViewModel
+
+class ViewModelFactory(
+    private val repository: BaseRepository
+
+) : ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(ApodViewModel::class.java) -> ApodViewModel(repository as ApodRepository) as T
+            else -> throw IllegalArgumentException("ViewModel Not Found")
+        }
+    }
+}
